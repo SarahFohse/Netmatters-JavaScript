b8d9924175866adce15c823999cc660af0b82855
SarahFohse <sarahfohse@gmail.com>
2023-10-25 10:50:50 +0100

    making sure all php includes are working on any of the pages

---
+++ contact.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Netmatters - Contact Us</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="icon" href="./favicon.ico" type="image/x-icon">
+	<script src="https://kit.fontawesome.com/5cbcf5da8c.js" crossorigin="anonymous"></script>
+</head>
+  <body id="stop-scrolling" class="">
+	  <div id="close-side-menu"></div>
+	  <?php include("inc/consent-message.php"); ?>
+	  <?php include("inc/header.php"); ?>
+    <main>
+        <h1>Welcome to My Website</h1>  
+    </main>
+    
+    <?php include("inc/footer.php"); ?>
+		<?php include("inc/side-menu.php"); ?>
+		<?php include("inc/cookie.php"); ?>
+		
+	<script src="javascript/main.js"></script>
+  </body>
+</html>
---
+++ inc/consent-message.php
@@ -0,0 +1,6 @@
+<div class="message">
+  <button type="button"><i class="fa-solid fa-message"></i></button>
+</div>
+<div class="consent">
+  <button type="button" id="consent-btn">Manage Consent</button>
+</div>
