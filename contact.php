8f970cb219f4a92fe8bfa19c3027b6e19605882d
SarahFohse <sarahfohse@gmail.com>
2023-10-26 09:47:02 +0100

    set up contact page

--- contact.php
+++ contact.php
@@ -17,7 +17,68 @@
 	  <?php include("inc/consent-message.php"); ?>
 	  <?php include("inc/header.php"); ?>
     <main>
-        <h1>Welcome to My Website</h1>  
+
+    <div class="container">
+      <div class="office-tab">
+        <div class="office-info">
+          <img src="images/london.jpg" alt="London Office">
+          <h3>London Office</h3>
+          <p>Unit G6,<br>Pixel Business Center,<br>110 Brooker Road, Waltham Abbey,<br>London,<br>EN91JH</p>
+          <a href="#">02045397354</a>
+          <div>
+            <a href="#">View More</a>
+          </div>
+        </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11767.910425271419!2d-0.01523071646948471!3d51.68000233203411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761fe63fd90547%3A0x59ebdd0b2c47b21c!2sNetmatters%20London!5e0!3m2!1sen!2suk!4v1694613203146!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="map"></iframe>
+      </div>
+
+      <div class="office-tab">
+        <div class="office-info">
+          <img src="images/london.jpg" alt="London Office">
+          <h3>London Office</h3>
+          <p>Unit G6,<br>Pixel Business Center,<br>110 Brooker Road, Waltham Abbey,<br>London,<br>EN91JH</p>
+          <a href="#">02045397354</a>
+          <div>
+            <a href="#">View More</a>
+          </div>
+        </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2443.4463841206734!2d0.15128277704004142!3d52.23527525727029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8711469d7de59%3A0x4ad66f1b36a452da!2sNetmatters%20Cambridge!5e0!3m2!1sen!2suk!4v1694613141715!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="map"></iframe>
+      </div>
+    
+      <div class="office-tab">
+        <div class="office-info">
+          <img src="images/london.jpg" alt="London Office">
+          <h3>London Office</h3>
+          <p>Unit G6,<br>Pixel Business Center,<br>110 Brooker Road, Waltham Abbey,<br>London,<br>EN91JH</p>
+          <a href="#">02045397354</a>
+          <div>
+            <a href="#">View More</a>
+          </div>
+        </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2424.644379747!2d1.1339954766845473!3d52.576042072076014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d9ddac8dba0b4b%3A0x9c77ffbfe7911dab!2sNetmatters!5e0!3m2!1sen!2suk!4v1692192981237!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="map"></iframe>
+      </div>
+
+    
+      <div class="office-tab">
+        <div class="office-info">
+          <img src="images/london.jpg" alt="London Office">
+          <h3>London Office</h3>
+          <p>Unit G6,<br>Pixel Business Center,<br>110 Brooker Road, Waltham Abbey,<br>London,<br>EN91JH</p>
+          <a href="#">02045397354</a>
+          <div>
+            <a href="#">View More</a>
+          </div>
+        </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1715.2766080459753!2d1.712628985995402!3d52.55573141109476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47da0593b311cec3%3A0x1cb3c1d4c0b340f6!2sNetmatters%20Great%20Yarmouth!5e0!3m2!1sen!2suk!4v1692193267187!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="map"></iframe>
+      </div>
+
+    
+
+
+
+    </div>
+
+    
     </main>
     
     <?php include("inc/footer.php"); ?>
